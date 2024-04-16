80e938d1f5ded7cef1be71c636bafd9e2d036e29
OLGUO <143014722+OLGUO@users.noreply.github.com>
2024-04-15 22:20:51 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -28,7 +28,7 @@ public class OnlinePurchaseManager
     for(int i = 0; i < purchases.size()-1; i++)
       if(purchases.get(i).equals(purchases.get(i+1)))
         return true;
-    retun false;
+    return false;
   }
   public OnlinePurchaseManager()
   {
